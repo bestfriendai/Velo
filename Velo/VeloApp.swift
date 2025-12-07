8bd2285f1859756c0b4475ffb758f9b6ff6e4a9c
kyvu <kyvu@tektrify.com>
2025-12-06 23:15:42 -0800

    Initial Commit

---
+++ Velo/VeloApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloApp.swift
+//  Velo
+//
+//  Created by Ky Vu on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct VeloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
